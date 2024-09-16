aa01e6e0ac87229f5430ccb9687367e7820c6e65
Manuel Maldonado <manuelmaldonado2898@gmail.com>
2024-09-16 06:00:31 -0400

    Agregando ejemplo del swagger al dto de proyectos

--- src/proyectos/dto/create-proyecto.dto.ts
+++ src/proyectos/dto/create-proyecto.dto.ts
@@ -1,18 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class CreateProyectoDto {
+  @ApiProperty({ example: 'Sistema de Gestión de Residuos Sólidos' })
   @IsString()
   nombre: string;
 
+  @ApiProperty({ example: 'Este sistema se basó en ...' })
   @IsString()
   descripcion: string;
 
+  @ApiProperty({ example: 'Url de la portada' })
   @IsString()
   portada: string;
 
+  @ApiProperty({ example: 'admin' })
   @IsString()
   agregado_por: string;
 
+  @ApiProperty({ example: '2020-01-01' })
   @IsString()
   agregado_en: string;
 }
