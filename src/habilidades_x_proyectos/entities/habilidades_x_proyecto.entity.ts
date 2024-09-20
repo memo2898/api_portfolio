8a5a61c45f230a7ce1e2f6374912df8a420a0b52
Manuel Maldonado <manuelmaldonado2898@gmail.com>
2024-09-19 21:21:12 -0400

    habilidades x proy listo

--- src/habilidades_x_proyectos/entities/habilidades_x_proyecto.entity.ts
+++ src/habilidades_x_proyectos/entities/habilidades_x_proyecto.entity.ts
@@ -1,4 +1,12 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Habilidade } from 'src/habilidades/entities/habilidade.entity';
+import { Proyecto } from 'src/proyectos/entities/proyecto.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity('tbld_habilidades_x_proyectos')
 export class HabilidadesXProyecto {
@@ -8,8 +16,16 @@ export class HabilidadesXProyecto {
   @Column()
   id_proyecto: number;
 
+  @OneToOne(() => Proyecto, (proyecto) => proyecto, { eager: true })
+  @JoinColumn({ name: 'id_proyecto' })
+  proyecto: Proyecto;
+
   @Column()
   id_habilidad: number;
+
+  @OneToOne(() => Habilidade, (habilidad) => habilidad, { eager: true })
+  @JoinColumn({ name: 'id_habilidad' })
+  habilidad: Habilidade;
 
   @Column()
   estado: string;
